180901ca479281c4eba15232672527b9849d6b52
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-29 15:27:47 +0330

    client get customers method implemented

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package client
 import (
 	"cmd/customer"
 	"context"
+	"io"
 	"log"
 )
 
@@ -17,5 +18,22 @@ func createCustomer(client customer.CustomerClient, customer *customer.CustomerR
 	}
 	if resp.Success {
 		log.Printf("A new Customer has been added with id: %d", resp.Id)
+	}
+}
+
+func getCustomers(client customer.CustomerClient, filter *customer.CustomerFilter) {
+	stream, err := client.GetCustomers(context.Background(), filter)
+	if err != nil {
+		log.Fatalf("Error on get customers: %v", err)
+	}
+	for {
+		rec, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetCustomers(_) = _, %v", client, err)
+		}
+		log.Printf("Customer: %v", rec)
 	}
 }
