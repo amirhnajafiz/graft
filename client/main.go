27d14960f94cc7833695ec20dfac41ee674e7d18
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-29 15:24:08 +0330

    client testing init with create

--- client/main.go
+++ client/main.go
@@ -1,1 +1,21 @@
 package client
+
+import (
+	"cmd/customer"
+	"context"
+	"log"
+)
+
+const (
+	address = "localhost:8080"
+)
+
+func createCustomer(client customer.CustomerClient, customer *customer.CustomerRequest) {
+	resp, err := client.CreateCustomer(context.Background(), customer)
+	if err != nil {
+		log.Fatalf("Could not create Customer: %v", err)
+	}
+	if resp.Success {
+		log.Printf("A new Customer has been added with id: %d", resp.Id)
+	}
+}
