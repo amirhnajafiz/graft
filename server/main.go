3117d68a47be8df90ae672adecf603e83251c464
amirhnajafiz <najafizadeh21@gmail.com>
2022-01-29 14:20:18 +0330

    server started on port, ready for endpoints

--- server/main.go
+++ server/main.go
@@ -1,6 +1,12 @@
-package server
+package main
 
-import "cmd/customer"
+import (
+	"cmd/customer"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
 const (
 	port = ":8080"
@@ -8,4 +14,26 @@ const (
 
 type Server struct {
 	savedCustomers []*customer.CustomerRequest
+}
+
+func (s *Server) CreateCustomer(ctx context.Context, in *customer.CustomerRequest) (*customer.CustomerResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) GetCustomers(filter *customer.CustomerFilter, stream customer.Customer_GetCustomersServer) error {
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	customer.RegisterCustomerServer(s, &Server{})
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to create server: %v", err)
+	}
 }
