c91726dff208dab8b6f860bd2e691544d17abea6
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-23 09:29:43 +0430

    fix: server bug

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"strings"
 
 	"github.com/amirhnajafiz/protocol-buffers/proto"
 	"google.golang.org/grpc"
@@ -11,6 +13,34 @@ import (
 const (
 	port = ":8080"
 )
+
+type server struct {
+	savedCustomers []*proto.CustomerRequest
+}
+
+func (s *server) CreateCustomer(_ context.Context, in *proto.CustomerRequest) (*proto.CustomerResponse, error) {
+	s.savedCustomers = append(s.savedCustomers, in)
+
+	log.Println("Customer created")
+
+	return &proto.CustomerResponse{Id: in.Id, Success: true}, nil
+}
+
+func (s *server) GetCustomers(filter *proto.CustomerFilter, stream proto.Customer_GetCustomersServer) error {
+	for _, savedCustomer := range s.savedCustomers {
+		if filter.Keyword != "" {
+			if !strings.Contains(savedCustomer.Name, filter.Keyword) {
+				continue
+			}
+		}
+
+		if err := stream.Send(savedCustomer); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", port)
