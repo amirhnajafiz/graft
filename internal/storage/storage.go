10b03ec0bef48c136c74c28d034d344acc616e91
amirhnajafiz <najafizadeh21@gmail.com>
2024-09-24 16:29:09 -0400

    add: storage component

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,16 @@
 package storage
+
+import "github.com/amirhnajafiz/bpb/pkg/rpc/logs"
+
+type Storage interface {
+	Append(log *logs.Log)
+	Fetch() []*logs.Log
+}
+
+func NewStorage() Storage {
+	s := storage{
+		logs: make([]*logs.Log, 0),
+	}
+
+	return &s
+}
